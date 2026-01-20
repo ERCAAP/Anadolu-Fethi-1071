176398967572d5f340f80d0c359f12acd58d699e
asdasd <96483522+ERCAAP@users.noreply.github.com>
2026-01-20 06:10:23 +0300

    update

---
+++ Assets/-SCRIPT/GameStateMachine/States/MainMenuState.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using AnadoluFethi.StateMachine;
+
+namespace AnadoluFethi.GameStateMachine
+{
+    public class MainMenuState : BaseState
+    {
+        private readonly GameStateMachineController _controller;
+
+        public MainMenuState(StateMachineController stateMachine, GameStateMachineController controller)
+            : base(stateMachine)
+        {
+            _controller = controller;
+        }
+
+        public override void Enter()
+        {
+            // Called when entering this state
+        }
+
+        public override void Update()
+        {
+            // Called every frame while in this state
+        }
+
+        public override void FixedUpdate()
+        {
+            // Called every fixed update while in this state
+        }
+
+        public override void Exit()
+        {
+            // Called when exiting this state
+        }
+    }
+}
